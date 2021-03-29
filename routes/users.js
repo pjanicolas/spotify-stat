52725ddace6ebc1858d8b1d300c4b26a0015eb55
Peter John Nicolas <58547325+pjanicolas@users.noreply.github.com>
2021-03-29 17:22:59 +0800

    Revert "Feature/redis"

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const controller = require('../controller/index');
-
-const objectController = new controller();
 
 /* GET users listing. */
-router.get('/:userid', function(req, res, next) {
-  return objectController.distribute('user', 'view', req, res);
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
 module.exports = router;
